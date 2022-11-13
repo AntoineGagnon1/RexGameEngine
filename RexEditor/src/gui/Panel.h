bc51711220c2502ef9b84d177c692be78489294d
Antoine Gagnon <antoinegagnon123@gmail.com>
2022-11-13 04:04:05 -0500

    Forgot Panel.h in last commit

---
+++ RexEditor/src/gui/Panel.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+
+namespace RexEditor
+{
+	class Panel
+	{
+	public:
+		Panel(const std::string& title);
+		virtual ~Panel() { }
+
+		void Render(float deltaTime);
+
+		void Hide();
+		void Show();
+
+		Vector2Int PanelSize() { return m_size; }
+		bool IsFocused() { return m_focused; }
+
+	protected:
+		virtual void OnGui(float deltaTime) = 0;
+		virtual void OnResize(Vector2Int oldSize, Vector2Int newSize) {};
+		virtual void OnFocusEnter() {};
+		virtual void OnFocusLeave() {};
+
+	private:
+		std::string m_title;
+		Vector2Int m_size;
+		bool m_open;
+		bool m_focused;
+	};
+}
