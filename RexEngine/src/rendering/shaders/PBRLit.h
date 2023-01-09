cdf8af5b71017ee14d1da4fd0ad4ec44710d1ccc
Antoine Gagnon <antoinegagnon123@gmail.com>
2023-01-08 22:57:51 -0500

    Moved lighting functions to #using pbr

--- RexEngine/src/rendering/shaders/PBRLit.h
+++ RexEngine/src/rendering/shaders/PBRLit.h
@@ -34,140 +34,14 @@ in vec3 normal;
 in vec3 worldPos;
 out vec4 FragColor; 
 
-const float PI = 3.14159265359;
-
 [Slider(0, 1)]uniform vec3  albedo;
 [Slider(0, 1)]uniform float metallic;
 [Slider(0, 1)]uniform float roughness;
 [Slider(0, 1)]uniform float ao;
 
-float DistributionGGX(vec3 N, vec3 H, float roughness)
-{
-    float a = roughness * roughness;
-    float a2 = a * a;
-    float NdotH = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH * NdotH;
-
-    float num = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
-
-    return num / denom;
-}
-
-float GeometrySchlickGGX(float NdotV, float roughness)
-{
-    float r = (roughness + 1.0);
-    float k = (r * r) / 8.0;
-
-    float num = NdotV;
-    float denom = NdotV * (1.0 - k) + k;
-
-    return num / denom;
-}
-
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
-{
-    float NdotV = max(dot(N, V), 0.0);
-    float NdotL = max(dot(N, L), 0.0);
-    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
-    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
-
-    return ggx1 * ggx2;
-}
-
-vec3 fresnelSchlick(float cosTheta, vec3 F0)
-{
-    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
-}
-
-vec3 fresnelSchlickRoughness(float cosTheta, vec3 F0, float roughness)
-{
-    return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
-}
-
-// Returns the radiance, for Point light and directional lights
-float CalculateLight(LightData light, vec3 worldPos, vec3 N, vec3 V, vec3 F0)
-{
-    vec3 L, H, radiance;
-    if (light.Pos.w == 1.0f)
-    { // Point light
-        /*L = normalize(light.Pos.xyz - worldPos);
-        H = normalize(V + L);
-        float distance = length(light.Pos.xyz - worldPos);
-        float attenuation = 1.0 / (distance * distance);
-        radiance = light.Color * attenuation;*/
-        L = normalize(-light.Pos.xyz); // .Pos is the direction
-        H = normalize(V + L);
-        radiance = light.Color;
-    }
-    else
-    { // Directional light
-        L = normalize(-light.Pos.xyz); // .Pos is the direction
-        H = normalize(V + L);
-        radiance = light.Color;
-    }
-
-    // cook-torrance brdf
-    float NDF = DistributionGGX(N, H, roughness);
-    float G = GeometrySmith(N, V, L, roughness);
-    vec3 F = fresnelSchlick(max(dot(H, V), 0.0), F0);
-
-    vec3 kS = F;
-    vec3 kD = vec3(1.0) - kS;
-    kD *= 1.0 - metallic;
-
-    vec3 numerator = NDF * G * F;
-    float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001;
-    vec3 specular = numerator / denominator;
-
-    // Outgoing radiance
-    float NdotL = max(dot(N, L), 0.0);
-    return (kD * albedo / PI + specular) * radiance * NdotL;
-}
-
 void main()
-{ 
-    vec3 N = normalize(normal);
-    vec3 V = normalize(cameraPos - worldPos);
-    vec3 R = reflect(-V, N);
-
-    vec3 F0 = vec3(0.04); // Min value for non-metal surfaces
-    F0 = mix(F0, albedo, metallic);
-
-    // reflectance equation
-    vec3 Lo = vec3(0.0);
-
-    // calculate per-light radiance
-    for (int i = 0; i < LightCount; i++)
-        Lo += CalculateLight(Lights[i], worldPos, N, V, F0);
-
-    // Ambient
-    vec3 F = fresnelSchlickRoughness(max(dot(N, V), 0.0), F0, roughness);
-
-    vec3 kS = F;
-    vec3 kD = 1.0 - kS;
-    kD *= 1.0 - metallic;
-
-    vec3 irradiance = texture(PBRIrradianceMap, N).rgb;
-    vec3 diffuse = irradiance * albedo;
-
-    // Specular
-    const float MAX_REFLECTION_LOD = 4.0;
-    vec3 prefilteredColor = textureLod(PBRPrefilterMap, R, roughness * MAX_REFLECTION_LOD).rgb;
-    vec2 brdf = texture(PBRBrdfLUT, vec2(max(dot(N, V), 0.0), roughness)).rg;
-    vec3 specular = prefilteredColor * (F * brdf.x + brdf.y);
-
-    vec3 ambient = (kD * diffuse + specular) * ao;
-
-    vec3 color = ambient + Lo;
-
-    // HDR tonemapping
-    color = color / (color + vec3(1.0));
-    // gamma correct
-    color = pow(color, vec3(1.0 / 2.2));
-
-    FragColor = vec4(color, 1.0);
+{
+    FragColor = vec4(GetPBRColorLit(albedo, metallic, roughness, ao, worldPos, cameraPos, normal), 1.0);
 }
 	)";
 }
